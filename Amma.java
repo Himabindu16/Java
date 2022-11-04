3ad1406bb114d77fe54496bea0140e50544e0b0e
Kadali Himabindu <67783064+Himabindu16@users.noreply.github.com>
2022-11-04 21:56:42 +0530

    Add files via upload

---
+++ Amma.java
@@ -0,0 +1,87 @@
+
+public class Amma {
+
+	public static void main(String[] args) {
+		int n=10;
+		for(int i=0;i<n;i++) {
+			
+			//K
+			for(int j=0;j<n;j++) {
+				if(j==0||2*i+j==(n-1)||2*i-j==(n-1)&&i>(n-1)/2) 
+				System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.print(" ");
+			
+			//.
+			for(int j=0;j<n;j++) {
+				if(i==(n-1)/2&&j==(n-1)/2)
+					System.out.print("*");
+				else
+					System.out.print(" ");	
+			}
+			System.out.print(" ");			
+				
+			//J
+			for(int j=0;j<n;j++) {
+				if(i==0||i==n-1&&j<(n-1)/2||j==(n-1)/2&&i<n-1) 
+				System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.print(" ");
+			
+			//Y
+			for(int j=0;j<n;j++) {
+			if(j==(n-1)/2&&i>(n-1)/2||i==j&&i<=(n-1)/2||i+j==n-1&&i<=(n-1)/2)	
+					System.out.print("*");
+			else
+				System.out.print(" ");
+			}
+				System.out.print("  ");
+			
+			//O
+			for(int j=0;j<n;j++) {
+				if(i==0&&j>0&&j<n-1||i>0&&i<n-1&&j==0||i==n-1&&j>0&&j<n-1||i>0&&i<n-1&&j==n-1) 
+				System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.print(" ");		
+			
+			//T
+			for(int j=0;j<n;j++) {
+				if(i==0||j==(n-1)/2) 
+				System.out.print("*");
+				else
+				System.out.print(" ");
+			}
+			System.out.print(" ");
+			
+			//H
+			for(int j=0;j<n;j++) {
+				if(j==0||i==(n-1)/2||j==n-1)				
+					System.out.print("*");	
+				else
+					System.out.print(" ");
+			}
+			System.out.print("  ");
+			
+			//I
+			for(int j=0;j<n;j++) {    
+			if(i==0||i==n-1||j==(n-1)/2)			
+				System.out.print("*");
+			else
+				System.out.print(" ");
+			}
+			System.out.print("  ");
+			
+			
+			
+			System.out.println(" ");
+			
+		}
+	}
+
+}
---
+++ NClass.java
@@ -0,0 +1,20 @@
+
+
+public class NClass {
+
+	public static void main(String[] args) {
+
+		int n=10;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				
+				if(j==0||j==n-1||i==j)	
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
